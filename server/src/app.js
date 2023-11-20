abbfe2b0eadd4315647e2b6ce3bdbdfb0ac6e195
hanishbansal231 <hanishbansal803@gmail.com>
2023-11-20 23:48:44 +0530

    Update basic setup and connect db

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,30 @@
 import express from "express";
-
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app = express();
 
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({extended:true,limit:'16kb'}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get('/',(req,res) => {
+    return res.status(200).json({
+        message:`WELCOME REAL ESTATE...`
+    });
+});
+
+app.all('*',(req,res) => {
+    return res.status(404).json({
+        message:`oops!page not found...`
+    });
+});
 
 
 export default app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
 import app from './app.js';
-
-
-app.listen(8080,() => {
-    console.log(`Server is running...`);
+import dbConnection from './config/dbConnection.js';
+import {config} from 'dotenv';
+config({
+    path:'./env'
 });
+
+const PORT = process.env.PORT || 5000;
+
+dbConnection()
+.then(() => {
+    app.listen(PORT,() => {
+        console.log(`⚙️  Server is running at port : ${PORT}`);
+    });
+})
+.catch((Error) => {
+    console.log('MONGODB CONNECTION FAILED -> ', Error);
+})
