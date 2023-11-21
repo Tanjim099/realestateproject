9328dd15cb5b18cf0b31b770ca86a31cd01ebf4f
Tanjim <tanjimseo@gmail.com>
2023-11-21 10:53:07 +0530

    Added auth Model

---
+++ server/src/models/authModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const authSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const authModel = mongoose.model("auth", authSchema);
+export default authModel
