f8fbdb4ffc0f77fd8243daf2af89317299721116
Tanjim <tanjimseo@gmail.com>
2023-11-24 00:29:14 +0530

    Added Home Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomeLayout from './components/HomeLayout'
 
 function App() {
   return (
-    <>
-      <h1 className=' text-red-600'>Hello</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomeLayout />} />
+    </Routes>
   )
 }
 
---
+++ client/src/components/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+
+function HomeLayout({ children }) {
+    return (
+        <div>
+            <Navbar />
+            <main className=" min-h-[90vh]">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default HomeLayout
