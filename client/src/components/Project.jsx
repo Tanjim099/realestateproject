b61526e6d9f1be769158f9a4d100a9981d980399
hanishbansal231 <hanishbansal803@gmail.com>
2023-11-27 19:03:30 +0530

    Updates in homepage

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,5 +1,6 @@
 import "../styles/Project.css";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
+// import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
+import { FaBed } from "react-icons/fa";
 function Project() {
     let projects = document.getElementById("projects");
     function arrowLeft() {
@@ -12,25 +13,41 @@ function Project() {
     return (
         <div>
             <div className="project_container w-100  relative">
-                <div className="sliderBtn top-1/2">
+                {/* <div className="sliderBtn top-1/2">
 
                     <button onClick={arrowRight} className=" absolute left-[-1%] md:left-[0%] top-1/2 w-[40px] h-[40px] bg-[#cff4ff] rounded-full text-center flex items-center justify-center"><IoIosArrowBack /></button>
                     <button onClick={arrowLeft} className=" absolute right-[-1%] md:right-[0%] top-1/2 w-[40px] h-[40px] bg-[#cff4ff] rounded-full text-center flex items-center justify-center"><IoIosArrowForward /></button>
-                </div>
+                </div> */}
                 <div className="projects flex overflow-x-auto gap-5" id="projects">
-                    <div className="min-w-[300px] p-3 border-2">
-                        <div className="project_header">
-                            <img className="w-full" src="https://www.homznspace.com/wp-content/uploads/2023/11/Main-Elevation-Brigade-Sanctuary-324x235.jpg" alt="" />
+
+                    <div className="min-w-[400px] border">
+                        <div className="project_header h-[200px]">
+                            <img className="w-full h-full object-cover" src="https://www.homznspace.com/wp-content/uploads/2023/11/Main-Elevation-Brigade-Sanctuary-324x235.jpg" alt="" />
                         </div>
-                        <div className="project_content mt-2">
-                            <h3 className="project_name text-lg font-medium">Orchid The Consulate Apartments</h3>
-                            <p className="project_description text-sm">3,4 BHK Apartment in Dhul Siras, Dwarka Delhi</p>
+                        <div className="project_content mt-2 px-6 py-4">
                             <div className="flex justify-between">
-                                <span className=" font-medium">₹ 3.12 Cr</span>
-                                <button className="px-2 py-1 rounded-sm  border-2">Details</button>
+                                <span className="font-medium text-xl font-sans">₹ 3.12 Cr</span>
+                                <button className="px-2 py-1 rounded  border-2">Details</button>
+                            </div>
+                            <h5 className="text-2xl my-1">Appartment</h5>
+                            <p className="font-mono mt-3">Get started by choosing from one of our pre-built page templates to showcase your properties</p>
+                        </div>
+                        <div className="border-t-2 flex justify-between px-5 py-4">
+                            <div className="flex items-center justify-center gap-1">
+                                <span><FaBed /></span>
+                                <span>2 Bedroom</span>
+                            </div>
+                            <div className="flex items-center justify-center gap-1">
+                                <span><FaBed /></span>
+                                <span>2 Bedroom</span>
+                            </div>
+                            <div className="flex items-center justify-center gap-1">
+                                <span><FaBed /></span>
+                                <span>2 Bedroom</span>
                             </div>
                         </div>
                     </div>
+
                     <div className="min-w-[300px] p-3 border-2">
                         <div className="project_header">
                             <img className="w-full" src="https://www.homznspace.com/wp-content/uploads/2023/11/Main-Elevation-Brigade-Sanctuary-324x235.jpg" alt="" />
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import HomeLayout from "../components/HomeLayout";
 import Project from "../components/Project";
 import { getAllProjects } from "../redux/slices/projectSlice";
 import { useEffect } from "react";
+import backroundImage from '../assets/items1.jpg';
+import image from '../assets/section-bg1.jpg';
+import { MdOutlineHomeWork } from "react-icons/md";
 
 function HomePage() {
     const dispatch = useDispatch();
@@ -20,7 +23,19 @@ function HomePage() {
         <HomeLayout>
             <div className="homeContainer ">
                 <div className="hero_section">
-                    <div className="hero h-[500px] sm:h-[200px] md:h-[300px]" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+                    <div className="hero min-h-[600px] sm:h-[200px] md:h-[300px]" style={{
+                        backgroundImage: `url(${backroundImage})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundAttachment: 'fixed',
+                        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+                        borderRadius: '10px',
+                        opacity: 0.9,
+                        transition: 'background 0.3s ease-in-out',
+                        clipPath: 'polygon(0 0, 100% 0, 100% 90%, 0 100%)',
+                    }}>
                         <div className="hero-overlay bg-opacity-60"></div>
                         <div className="hero-content text-center text-neutral-content w-[100%]">
 
@@ -35,10 +50,42 @@ function HomePage() {
                     </div>
                 </div>
                 <div className="project_section sm:w-[80%] md:w-[80%] m-auto mt-5">
-                    <h1 className=" text-3xl font-semibold mb-3">Top Projects</h1>
+                    <h1 className=" text-3xl font-semibold mb-3 border-b-4 pb-4 border-[#7f1657]">Top Projects</h1>
                     <div className=" my-5">
                         <Project />
                     </div>
+                </div>
+                <div className="h-[500px] my-10 flex flex-wrap items-center flex-col justify-center" style={{
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundAttachment: 'fixed',
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+                    borderRadius: '10px',
+                    opacity: 0.9,
+                    transition: 'background 0.3s ease-in-out',
+                }}>
+                    <h2 className="text-white text-4xl text-center">How we help people?</h2>
+                    <div className="bg-[#7f1657] mt-10 px-4 flex justify-between flex-wrap text-white h-[300px] w-[70%]">
+                        <div className="text-center w-[30%] flex flex-col items-center justify-center p-2">
+                            <p className="text-[60px]"><MdOutlineHomeWork /></p>
+                            <h3 className="text-xl my-3 font-bold">Sell home or office</h3>
+                            <p className="font-mono leading-6">Get started by choosing from one of our pre-built page templates to showcase your properties</p>
+                        </div>
+                        <div className="text-center w-[30%] flex flex-col items-center justify-center p-2">
+                            <p className="text-[60px]"><MdOutlineHomeWork /></p>
+                            <h3 className="text-xl my-3 font-bold">Rent home or office</h3>
+                            <p className="font-mono leading-6">Get started by choosing from one of our pre-built page templates to showcase your properties</p>
+                        </div>
+                        <div className="text-center w-[30%] flex flex-col items-center justify-center p-2">
+                            <p className="text-[60px]"><MdOutlineHomeWork /></p>
+                            <h3 className="text-xl my-3 font-bold">Find next</h3>
+                            <p className="font-mono leading-6">Get started by choosing from one of our pre-built page templates to showcase your properties</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </HomeLayout>
