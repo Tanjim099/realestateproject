d1e4b3f5dfc7d664c9867d35ebc71e6e81bfa5f9
Tanjim <tanjimseo@gmail.com>
2023-12-03 00:52:21 +0530

    Added get Blogs controller and router

--- server/src/controllers/miscellaneousController.js
+++ server/src/controllers/miscellaneousController.js
@@ -1,6 +1,9 @@
-import authModel from "../models/authModel.js"
+import authModel from "../models/authModel.js";
+import blogModel from "../models/blogModel.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+
+//get all users
 export const allUsers = async (req, res, next) => {
     const totalUsers = await authModel.countDocuments();
     const page = parseInt(req.query.page) || 1;
@@ -16,7 +19,7 @@ export const allUsers = async (req, res, next) => {
     }
 }
 
-
+//delete user
 export const deleteUser = async (req, res, next) => {
     try {
         const { id } = req.params;
@@ -28,6 +31,21 @@ export const deleteUser = async (req, res, next) => {
             success: true,
             message: "User Delete Successfully"
         })
+    } catch (error) {
+        console.log(error);
+        next(new ApiError(500, error.message));
+    }
+}
+
+//get all blogs
+export const getAllBlogs = async (req, res, next) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const blogs = await blogModel.find().skip((page - 1) * limit).limit(limit).populate("author");
+        res.status(201).json(
+            new ApiResponse(200, blogs, "Blogs Fetched Successfully")
+        )
     } catch (error) {
         console.log(error);
         next(new ApiError(500, error.message));
--- server/src/routes/miscellaneousRoute.js
+++ server/src/routes/miscellaneousRoute.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { allUsers, deleteUser } from "../controllers/miscellaneousController.js";
+import { allUsers, deleteUser, getAllBlogs } from "../controllers/miscellaneousController.js";
 const router = Router();
 router.get("/admin/stat/users", allUsers);
-router.delete("/admin/stat/user/:id", deleteUser)
+router.delete("/admin/stat/user/:id", deleteUser);
+router.get("/admin/stat/blogs", getAllBlogs);
 
 export default router;
